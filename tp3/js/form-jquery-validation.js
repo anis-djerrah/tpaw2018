acf2f0edd6aaddb89f3255469d784c4b59b8d203
djerrah anis <32833902+anis-djerrah@users.noreply.github.com>
2017-11-14 01:30:55 +0100

    Add files via upload

---
+++ tp3/js/form-jquery-validation.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function () {
+    console.log("DOM ready!");
+    $("button").click(function (event) {
+        event.preventDefault();
+        if ($("#Inputnom").val() == ""
+            || $("#inputprenom").val() == ""
+            || $("#inputdatedenaissance").val() == ""
+            || $("#inputAdresse").val() == ""
+            || $("#InputEmail").val() == "") {
+            alert("veuillez remplir tous les champs")
+        } else {
+            $('#myModal').modal("show");
+            $(".modal-body").html('vous etes Mr '
+            +$("#Inputnom ").val()+' '
+            +$("#inputprenom ").val()
+            +' et vous etes nés le '
+            +$("#inputdatedenaissance").val()
+            +' et vous habitez à'+'<br/>'          
+            +'<img src="map.png" width="330" height="300"/>'+'<br/>'
+            +'<a href=https://www.google.fr/maps/@ target="_blank">localisez moi</a>'
+            
+        
+        
+        );
+        
+        
+            $('#myModal').modal("show")
+        
+           
+        }
+    });
+});
